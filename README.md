2c803f1e0caa5631aa5f66ad82dc051b98345141
tzaher <tenillec@gmail.com>
2018-06-14 22:30:50 -0500

    uploading udated code and README

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # liri-node-app
+
+LIRI Bot is a node.js application that runs in the terminal only.  
+
+The following commands are needed when it is fully implemented. 
+
+Step 1:
+
+my-tweets This will show the last 20 tweets and when they were created at in your terminal/bash window.
+
+==spotify-this-song==
+node liri.js spotify-this-song 'song title here' -This will show the following information about the song in your terminal/bash window: 
+* Artist(s) 
+* The song's name 
+* A preview link of the song from Spotify 
+* The album that the song is from
+
+==movie-this== 
+node liri.js movie-this 'title here' -This will output the following information to your terminal/bash window:
+
+* Title of the movie.
+* Year the movie came out.
+* IMDB Rating of the movie.
+* Rotten Tomatoes Rating of the movie.
+* Country where the movie was produced.
+* Language of the movie.
+* Plot of the movie.
+* Actors in the movie.
+
+==do-what-it-says== 
+node liri.js do-what-it-says
+
+-Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands. 
+-It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt. Feel free to change the text in that document to test out the feature for other commands.
---
+++ liri.js
@@ -0,0 +1,129 @@
+require("dotenv").config();
+
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var keys = require('./key.js');
+var request = require('request');
+var fs = require ('fs');
+//var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+
+console.log("Type my-tweets, spotify-this-song, movie-this, or do-what-it-says to get started!");
+// spotify-this-song
+// do-what-it-says
+
+//Function for my tweets 
+var getMyTweets = function () {
+    var client = new Twitter(keys.twitter);
+    var params = {
+        screen_name: "tnizzle32"
+    };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log("");
+                console.log(tweets[i].text);  
+            }
+        }
+    });
+};
+// Function for Spotify search
+var getArtistNames = function(artist) {
+    return artist.name;
+  };
+
+var getMySpotify = function(songName) {
+    if (songName === undefined) {
+      songName = "Run the World";
+    }
+    spotify.search(
+      {
+        type: "track",
+        query: songName
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return;
+        }
+        var songs = data.tracks.items;
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i);
+          console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+          console.log("song name: " + songs[i].name);
+          console.log("preview song: " + songs[i].preview_url);
+          console.log("album: " + songs[i].album.name);
+          console.log("-----------------------------------");
+        }
+      }
+    );
+  };
+// Function for Movie Search
+var getMyMovie = function(movieName) {
+    if (movieName === undefined) {
+      movieName = "Mr. Nobody";
+    }
+    var urlHit =
+      "http://www.omdbapi.com/?t=" +
+      movieName +
+      "&y=&plot=full&tomatoes=true&apikey=trilogy";
+    request(urlHit, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var jsonData = JSON.parse(body);
+        console.log("Title: " + jsonData.Title);
+        console.log("Year: " + jsonData.Year);
+        console.log("Rated: " + jsonData.Rated);
+        console.log("IMDB Rating: " + jsonData.imdbRating);
+        console.log("Rotton Tomatoes Rating: " + jsonData.Ratings[1].Value);
+        console.log("Country: " + jsonData.Country);
+        console.log("Language: " + jsonData.Language);
+        console.log("Plot: " + jsonData.Plot);
+        console.log("Actors: " + jsonData.Actors);
+      }
+    });
+  };
+// Function do-what-it-says command based on text file
+var doWhatItSays = function() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      console.log(data);
+      var dataArr = data.split(",");
+      if (dataArr.length === 2) {
+        pick(dataArr[0], dataArr[1]);
+      } else if (dataArr.length === 1) {
+        pick(dataArr[0]);
+      }
+    });
+  };
+
+// console.log(getMyTweets())
+// console.log(getMySpotify())
+// console.log(getMyMovie())
+// console.log(doWhatItSays())
+
+// Function for determining which command is executed
+var pick = function(caseData, functionData) {
+    switch (caseData) {
+      case "my-tweets":
+        getMyTweets();
+        break;
+      case "spotify-this-song":
+        getMySpotify(functionData);
+        break;
+      case "movie-this":
+        getMyMovie(functionData);
+        break;
+      case "do-what-it-says":
+        doWhatItSays();
+        break;
+      default:
+        console.log("LIRI doesn't know that");
+    }
+  };
+  // Function which takes in command line arguments and executes correct function accordigly
+  var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+  };
+  // MAIN PROCESS
+  // =====================================
+  runThis(process.argv[2], process.argv[3]);
